8829f2e2a43a0c5b7e93ec98cf00260e62fcfe4b
Oleg G <ogordienov@yandex.ru>
2023-08-02 18:31:50 +0200

    Add libs, configs and structure

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const pageData: Record<string, Record<string, string>> = {
+  '/index.html': {
+    cssFile: 'dasd',
+    greetings: 'Как дела?'
+  },
+};
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { resolve } from 'path';
+import { defineConfig, Plugin } from 'vite';
+import handlebars from 'vite-plugin-handlebars';
+import { pageData } from './src/constants';
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist')
+  },
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [
+    handlebars({
+      partialDirectory: [resolve(__dirname, 'src/components'), resolve(__dirname, 'src/pages')],
+      context(pagePath) {
+        return pageData[pagePath];
+      },
+    }) as Plugin
+  ],
+});
